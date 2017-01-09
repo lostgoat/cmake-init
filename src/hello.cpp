5ddbe0d3984e1f911a01df2b36d771be7995e7e9
Andres Rodriguez <andresx7@gmail.com>
2017-01-08 23:36:28 -0500

    Run make format

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,17 +1,17 @@
-#include <stdio>
 #include <myproject/config.h>
+#include <stdio>
 
 // Cpp-check sample from http://cppcheck.sourceforge.net/demo/
-void f1(struct fred_t *p)
+void f1 (struct fred_t *p)
 {
     // dereference p and then check if it's NULL
     int x = p->x;
     if (p)
-        do_something(x);
+        do_something (x);
 }
 
-int main()
+int main ()
 {
-	printf("Hello world %d.%d\n", MyProject_VERSION_MAJOR, MyProject_VERSION_MINOR);
-	return 0;
+    printf ("Hello world %d.%d\n", MyProject_VERSION_MAJOR, MyProject_VERSION_MINOR);
+    return 0;
 }
